6b29a3b0da71e33a6d8e92ffc487b3aedda1278a
mohammedbelgoumri <mohammedbelgoumri@gmail.com>
2023-03-13 11:12:29 +0100

    python

--- assets/code/mlp.py
+++ assets/code/mlp.py
@@ -1,13 +1,14 @@
-from typing import Sequence, Callable
+from typing import Sequence
+from numpy import array_split as blocks
 
 
-def embedding(input):
+def mlp(x):
     pass
 
 
-def mlp_sequence(input: Sequence) -> Sequence:
-    output = []
-    for element in input:
-        e = embedding(element)
-        output.append(e)
-    return output
+def mlp_s2s(xs: Sequence, k: int) -> Sequence:
+    ys = []  # Initialiser la sortie
+    for x in blocks(xs, k):  # Parcourir les blocs
+        y = mlp(x)  # Traiter chaque bloc
+        ys.append(y)  # Concatener les resultats
+    return ys
