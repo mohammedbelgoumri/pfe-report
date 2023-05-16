daeeb7cfbb53a8dd6d9a9666f95f98f62feebd9f
mohammedbelgoumri <mohammedbelgoumri@gmail.com>
2023-05-16 06:20:35 +0100

    hparams importance plot

--- assets/python/importance.py
+++ assets/python/importance.py
@@ -10,19 +10,35 @@ def main():
         "lr": {"Importance": 0.361, "Corrélation": -0.647},
     }
 
-    df = pd.DataFrame(df).T
-    fig, ax = plt.subplots(1, 2, sharey=True)
-    x = np.arange(len(df.index))
+    df = {
+        "metric": ["dropout", "lr", "dropout", "lr"],
+        "type": ["Importance", "Importance", "Corrélation", "Corrélation"],
+        "value": [0.639, 0.361, -0.899, -0.647],
+    }
+
+    df = pd.DataFrame(df)
+
     sns.set(style="whitegrid", palette="colorblind")
-    ax[0].barh(df.loc["dropout", "Importance"], width=0.2, label="Importance")
-    ax[0].barh(df.loc["dropout", "Corrélation"], width=0.2, label="Corrélation")
-    ax[1].barh(df.loc["lr", "Importance"], width=0.2, label="Importance")
-    ax[1].barh(df.loc["lr", "Corrélation"], width=0.2, label="Corrélation")
-    ax[0].set_title("Dropout")
-    ax[1].set_title("Learning rate")
+    fig, ax = plt.subplots(figsize=(6, 3))
+    sns.barplot(data=df, y="metric", x="value", hue="type", width=0.5, ax=ax)
+    ax.set_xlabel("")
+    ax.set_ylabel("")
     plt.legend()
     plt.tight_layout()
-    plt.show()
+    fig.savefig("assets/python/importance.pdf")
+
+    # fig, ax = plt.subplots(1, 2, sharey=True)
+    # x = np.arange(len(df.index))
+    # sns.set(style="whitegrid", palette="colorblind")
+    # ax[0].barh(df.loc["dropout", "Importance"], width=0.2, label="Importance")
+    # ax[0].barh(df.loc["dropout", "Corrélation"], width=0.2, label="Corrélation")
+    # ax[1].barh(df.loc["lr", "Importance"], width=0.2, label="Importance")
+    # ax[1].barh(df.loc["lr", "Corrélation"], width=0.2, label="Corrélation")
+    # ax[0].set_title("Dropout")
+    # ax[1].set_title("Learning rate")
+    # plt.legend()
+    # plt.tight_layout()
+    # plt.show()
 
 
 if __name__ == "__main__":
